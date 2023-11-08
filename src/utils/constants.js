65c17aa11cfe1903099d23a34bc904a8d27f6ba1
shubhanshurav <shubhanshurao7037@gmail.com>
2023-11-08 23:33:05 +0530

    Hiding the Keys

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,7 +7,7 @@ export const USER_AVTAR =
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer' + process.env.REACT_APP_TMDB_KEY,
+      Authorization: 'Bearer ' + process.env.REACT_APP_TMDB_KEY,
     }
   }; 
 
